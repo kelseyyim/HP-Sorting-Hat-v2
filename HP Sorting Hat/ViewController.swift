fff0f21c872066899f55a2ef9e871aa0e3d673af
Kelsey Yim <kelseyann.yim@gmail.com>
2019-02-27 19:02:39 -0800

    Initial Commit

---
+++ HP Sorting Hat/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HP Sorting Hat
+//
+//  Created by Kelsey Yim on 2/27/19.
+//  Copyright © 2019 Kelsey Yim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
